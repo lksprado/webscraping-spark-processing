3ebb795bd55a9896cf21bd95cf04de1cbdad3eae
lksprado <lks-prado@live.com>
2025-03-03 15:20:21 -0300

    formatting

--- app/extract.py
+++ app/extract.py
@@ -1,60 +1,57 @@
+import logging
+
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup as bs
 from requests.exceptions import ConnectionError
-import logging
-import pandas as pd 
 
 # CONFIGURACAO DO LOGGING
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
-    handlers=[
-        logging.FileHandler("app.log"), 
-        logging.StreamHandler()  
-    ]
+    handlers=[logging.FileHandler("app.log"), logging.StreamHandler()],
 )
 logger = logging.getLogger(__name__)
 
 
 def extract_html(url):
-    """ Extrai o HMTL de uma URL"""
+    """Extrai o HMTL de uma URL"""
     try:
         response = requests.get(url)
-        soup = bs(response.text,'html.parser')
+        soup = bs(response.text, "html.parser")
         logger.info(f"Extracao HTML realizada com sucesso --- {url}")
         return soup
     except ConnectionError as e:
         logger.error(f"Erro ao conectar url --- {e}")
 
+
 def extract_country_population_save_csv(soup):
     """Extrai dados da tabela no html"""
-    tab = soup.find('table')
-    linhas = tab.find_all('tr')
+    tab = soup.find("table")
+    linhas = tab.find_all("tr")
     countries = []
     for row in linhas:
         try:
-            data = row.find_all('td')
+            data = row.find_all("td")
             if data:
                 country = data[1].text.strip()
                 population = data[2].text.strip()
                 rate = data[3].text.strip()
-                countries.append({
-                    'country': country,
-                    'population': population,
-                    'rate': rate
-                })
+                countries.append(
+                    {"country": country, "population": population, "rate": rate}
+                )
         except AttributeError as e:
             logger.error(f"Erro ao extrair dados da tabela --- {e}")
             continue
-        
+
     countries = pd.DataFrame(countries)
-    countries.to_csv('populacao_paises.csv', index=False)
+    countries.to_csv("populacao_paises.csv", index=False)
     num_rows = len(countries)
     logger.info(f"Dados da tabela extraidos com sucesso --- {num_rows} linhas")
 
-if __name__ == '__main__':
-    URL = 'https://www.worldometers.info/world-population/population-by-country/'
+
+if __name__ == "__main__":
+    URL = "https://www.worldometers.info/world-population/population-by-country/"
 
     website_html = extract_html(URL)
     file = extract_country_population_save_csv(website_html)
-
--- main.py
+++ main.py
@@ -1,23 +1,22 @@
-from app.extract import extract_html
-from app.extract import extract_country_population_save_csv
-from app.transform import PopulationDataProcessor
 from pyspark.sql import SparkSession
 
+from app.extract import extract_country_population_save_csv, extract_html
+from app.transform import PopulationDataProcessor
+
+
 def main():
-    URL = 'https://www.worldometers.info/world-population/population-by-country/'
+    URL = "https://www.worldometers.info/world-population/population-by-country/"
 
     website_html = extract_html(URL)
     extract_country_population_save_csv(website_html)
-    
+
     spark = SparkSession.builder.appName("CSV to parquert").getOrCreate()
-    csv_path = "populacao_paises.csv"  
+    csv_path = "populacao_paises.csv"
     output_path = "populacao_paises_processado.parquet"
-    
-    processor = PopulationDataProcessor(spark, csv_path, output_path)   
+
+    processor = PopulationDataProcessor(spark, csv_path, output_path)
     processor.process()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-    
-    
-    
